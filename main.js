2e8f2df1be43b6762007d203fc8fccc5bff6cc4d
Andriy <dioxine2000@gmail.com>
2020-01-05 14:54:49 +0200

    Code reduction

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 let baseURL = 'https://api.themoviedb.org/3/';
-let APIKEY = '';  // Enter your apikey
+let APIKEY = '';  // Enter your apikey, you can get apikey here: 'https://www.themoviedb.org/'
 let baseImageURL = "https://image.tmdb.org/t/p/";
 let posterSize = 'w185';
 
 
-let getMainList = function (num) {
+let getMainList = function(num) {
+
   if (typeof(num) == 'object'){
     num = 1; 
-  }
-  if(output){
-  output.remove();
-  }
+  }  
+
   let mainPartUrl;
+
   if(window.location.hash == '#topRated'){
     mainPartUrl = 'tv/top_rated?api_key=';
-  }
-  else{
+  }else{
     mainPartUrl = 'tv/popular?api_key=';
   }
 
@@ -23,43 +22,59 @@ let getMainList = function (num) {
   fetch(url)
   .then(result=>result.json())
   .then((data)=>{
-    let smth = document.createElement('div');
-    smth.id = 'output';
-    page.append(smth);
+    refreshOutput();
+
     for (let show in data.results) {
       createTitle(show, data, output);
     }
+
     let lastp = data.total_pages;
     updatePagin(lastp, num);
   })
 }
 
 
-let createTitle = function (show, data, outputObj) {
-  let Name = data.results[show].name;
-  let TV_Show = document.createElement('li');
-  let TV = document.createElement('a');
-  TV.innerHTML = Name;
+let refreshOutput = function() {
+  output.remove();
+    let smth = document.createElement('div');
+    smth.id = 'output';
+    page.append(smth);
+}
+
+
+let createTitle = function(show, data, outputObj) {
+  let dataName = data.results[show];
+  let TV_Show = displayName(dataName);
+  TV = TV_Show.lastChild;
   TV.data = data;
   TV.show = show;
   TV.addEventListener('click', discription);
-  TV_Show.append(TV);
-  //TV_Show.append(createDiscription(Name, show, data));
+  
   outputObj.append(TV_Show);
 }
 
 
+let displayName = function(data) {
+  let name = data.name;
+  let object = document.createElement('div');
+  let objectName = document.createElement('a');
+  objectName.innerHTML = name;
+  object.append(objectName);
+  return object;
+}
+
+
 let discription = function() {
-  output.remove();
-  let smth = document.createElement('div');
-  smth.id = 'output';
-  page.append(smth);
+
+  refreshOutput();
+  pagination.style.display = 'none';
+
   let discrip = document.createElement('div');
   let name = document.createElement('div');
   name.innerHTML = this.innerHTML;
   discrip.append(name);
-  discrip.append(displayPoster(this.show, this.data));
-  discrip.append(displayOverview(this.show, this.data));
+  discrip.append(displayPoster(this.data.results[this.show]));
+  discrip.append(displayOverview(this.data.results[this.show]));
   let tvId = this.data.results[this.show].id;
   let url = "".concat(baseURL, 'tv/', tvId, 'popular?api_key=', APIKEY, '&language=en-US');
   fetch(url)
@@ -73,27 +88,30 @@ let discription = function() {
 }
 
 
-let displayOverview = function (show, data) {
-  let overviewShow = document.createElement('p');
-  let overview = data.results[show].overview;
-  overviewShow.innerHTML = overview;
-  return overviewShow;
-  }
+let displayOverview = function(data) {
+  let overviewObj = document.createElement('p');
+  let overview = data.overview;
+  overviewObj.innerHTML = overview;
+  return overviewObj;
+}
 
 
-let displayPoster = function (show, data, name) {
+let displayPoster = function(data) {
   let posterShow = document.createElement('img');
   posterShow.alt = 'Постер відсутній';
-  let posterPath = data.results[show].poster_path;
-  if (posterPath == null) {
-    return;
+  let posterPath = data.poster_path;
+  if (!(posterPath == null)) {
+    this.posterPath = posterPath;
+  }else{
+    posterPath = this.posterPath; // If not season's poster as a poster will used show's poster
   }
+
   posterShow.src = "".concat(baseImageURL, posterSize, posterPath);
   return posterShow;
-  }
+}
 
 
-let displayNumberOfSeasons = function (data) {
+let displayNumberOfSeasons = function(data) {
   let numberOfSeasons = document.createElement('p');
   let len = data.number_of_seasons;
   numberOfSeasons.innerHTML = 'Number of seasons: ' + len;
@@ -101,7 +119,7 @@ let displayNumberOfSeasons = function (data) {
 }
 
 
-let displayNumberOfEpisodes = function (data) {
+let displayNumberOfEpisodes = function(data) {
   let numberOfEpisodes = document.createElement('p');
   let len = data.number_of_episodes;
   numberOfEpisodes.innerHTML = 'Number of episodes: ' + len;
@@ -109,9 +127,10 @@ let displayNumberOfEpisodes = function (data) {
 }
  
 
-let displayListOfSeasons = function (data) {
+let displayListOfSeasons = function(data) {
   let listOfSeasons = document.createElement('div');
   let len = data.number_of_seasons;
+
   for (let i = 0; i < len; i++) {
     let nameOfSeason = data.seasons[i].name;
     let season = document.createElement('li');
@@ -121,63 +140,43 @@ let displayListOfSeasons = function (data) {
     season.addEventListener('click', seasonDiscription);
     seasonName.innerHTML = nameOfSeason;
     season.append(seasonName);
-    listOfSeasons.append(season);
-    
-  } 
+    listOfSeasons.append(season);  
+  }
+
   return listOfSeasons;
 }
 
 
 let seasonDiscription = function() {
-  output.remove();
-  let smth = document.createElement('div');
-  smth.id = 'output';
-  page.append(smth);
+  
+  refreshOutput();
+
   let discrip = document.createElement('div');
-  let name = document.createElement('div');
-  name.innerHTML = this.data.name;
-  discrip.append(name);
+
+  discrip.append(displayName(this.data));
+
   let numOfSeason = document.createElement('div');
   numOfSeason.innerHTML = 'Number of season: ' + this.numOfSeason;
   discrip.append(numOfSeason);
-  discrip.append(seasonPoster(this.data, this.numOfSeason));
-  discrip.append(seasonOveriew(this.data, this.numOfSeason));
+  discrip.append(displayPoster(this.data.seasons[this.numOfSeason]));
+  discrip.append(displayOverview(this.data.seasons[this.numOfSeason]));
   discrip.append(numOfEpisodesInSeason(this.data, this.numOfSeason));
-  discrip.append(listOfEpisodes(this.data, this.numOfSeason, seasonPoster(this.data, this.numOfSeason)));
+  discrip.append(listOfEpisodes(this.data, this.numOfSeason, displayPoster(this.data.seasons[this.numOfSeason])));
   output.append(discrip);
-  }
+}
 
 
-let seasonPoster = function(data, numOfSeason) {
-  let posterOfSeason = document.createElement('img');
-  posterOfSeason.alt = 'Постер відсутній';
-  let posterPath = data.seasons[numOfSeason].poster_path;
-  if (posterPath == null) {
-    return;
-  }
-  posterOfSeason.src = "".concat(baseImageURL, posterSize, posterPath);
-  return posterOfSeason;
-  }
-
-
- let seasonOveriew = function(data, numOfSeason){
-  let overviewOfSeason = document.createElement('p');
-  let overview = data.seasons[numOfSeason].overview;
-  overviewOfSeason.innerHTML = overview;
-  return overviewOfSeason;
- }
-
-
- let numOfEpisodesInSeason = function(data, numOfSeason) {
+let numOfEpisodesInSeason = function(data, numOfSeason) {
   let numberOfEpisodes = document.createElement('p');
   let len = data.seasons[numOfSeason].episode_count;
   numberOfEpisodes.innerHTML = 'Number of episodes: ' + len;
   return numberOfEpisodes;
- }
+}
 
 
-let listOfEpisodes = function(data, numOfSeason, seasonPoster){
+let listOfEpisodes = function(data, numOfSeason, seasonPoster) {
   let episodesList = document.createElement('div');
+
   for (let i = 1; i < data.seasons[numOfSeason].episode_count + 1; i++){
     let episode = document.createElement('li');
     let episodeName = document.createElement('a');
@@ -190,43 +189,39 @@ let listOfEpisodes = function(data, numOfSeason, seasonPoster){
     episode.append(episodeName);
     episodesList.append(episode);
   }
+
   return episodesList;
 }
 
 let episodeDiscriprion = function() {
-  output.remove();
-  let smth = document.createElement('div');
-  smth.id = 'output';
-  page.append(smth);
-  let showName = document.createElement('div');
-  name = this.data.name;
-  showName.innerHTML = name;
-  output.append(showName);
+  
+  refreshOutput();
+ 
+  output.append(displayName(this.data));
   output.append(this.poster);
   let id = this.data.id;
   let numS = this.numOfSeason;
+
   if (numS == 0) {
     numS = 1;
   }
+  
   let numE = this.num;
   let url = "".concat(baseURL, 'tv/', id, '/season/', numS, '/episode/', numE,  '?api_key=', APIKEY, '&language=en-US');
   fetch(url)
   .then(result=>result.json())
   .then((data)=>{
-    let overview = document.createElement('div');
-    overview.innerHTML = data.overview;
-    output.append(overview);
+    output.append(displayOverview(data));
     let episodeNum = document.createElement('div');
     let seasonNum = document.createElement('div');
     episodeNum.innerHTML = 'Number of episode: ' + data.episode_number;
     seasonNum.innerHTML = 'Number of season: ' + data.season_number;
     output.append(episodeNum, seasonNum);
   })
-
 }
 
 
-let createPaginNode = function (num, place) {
+let createPaginNode = function(num, place) {
   let p = document.createElement('a');
   p.num = num;
   p.innerHTML = num;
@@ -235,33 +230,39 @@ let createPaginNode = function (num, place) {
 }
 
 
-let pagin = function () {
+let pagin = function() {
   let center = this.num;
   getMainList(center);
 }
 
 
-let updatePagin = function (lastp, center) {
+let updatePagin = function(lastp, center) {
+
   if(pagination) {
     pagination.remove();
   }
+  
   let full = document.createElement('div');
   full.id = 'pagination';
+
   let before = document.createElement('div');
   before.id = 'beforeMain';
+  full.append(before);
+  
   let main = document.createElement('div');
   main.id = 'mainPart';
+  full.append(main);
+  
   let after = document.createElement('div');
   after.id = 'afterMain';
-  full. append(before);
-  full. append(main);
   full. append(after);
   body.append(full);
+
   let copyCenter = center;
+
   if (center < 3) {
     center = 3;
-  }
-  else if (center > lastp - 3) {
+  }else if (center > lastp - 3) {
     center = lastp - 2;
   }
 
@@ -270,14 +271,15 @@ let updatePagin = function (lastp, center) {
     afterMain.append(threep);
     createPaginNode(lastp, afterMain);
   }
-  if(!(copyCenter < 3)){
+
+  if(!(copyCenter < 3)) {
     createPaginNode(1, beforeMain);
     let threep = document.createTextNode('...');
     beforeMain.append(threep);
   }
 
-  for (let i = center - 2; i < center + 3; i++){
-  createPaginNode(i, mainPart);
+  for (let i = center - 2; i < center + 3; i++) {
+    createPaginNode(i, mainPart);
   }
 }
 
